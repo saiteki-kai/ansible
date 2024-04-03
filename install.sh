c5eb8d555674cde38338eaef9337b11864a25a34
saiteki-kai <giuseppe.magazu@gmail.com>
2024-04-03 20:31:17 +0200

    feat: ask to remove vault password file if present

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-ansible-playbook playbooks/laptop.yml --ask-become-pass --ask-vault-pass
+set -euo pipefail
+
+ansible-playbook playbooks/laptop.yml --ask-become-pass $@
+
+if [[ -f .vault_pass ]]; then
+    echo "Do you want to remove the vault password file? (y/n)"
+    read answer
+
+    if [ "$answer" == "y" ]; then
+        rm .vault_pass
+        echo ".vault_pass file removed successfully!"
+    fi
+
+    echo
+fi
+
+echo "Setup complete successfully!"
